aa1a701f26e23a73be25309fec080bf761fb28a0
Sherline Kinyanjui <sherlinekinyanjui@gmail.com>
2021-02-16 16:21:14 +0300

    Update views.py

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -7,7 +7,7 @@ def home():
     """Renders the home page."""
     # TODO: Create a query parameter so as to add our name to the URL like 
     # https://first-project.azurewebsites.net/name=Grace
-    name = "</>"
+    name = "Sherline"
 
     return render_template(
         'index.html',
